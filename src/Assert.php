0955b38007017328b47b496ac24983768e129ea3
Realodix <budi03@gmail.com>
2022-06-25 16:43:16 +0700

    Update Assert.php

--- src/Assert.php
+++ src/Assert.php
@@ -55,7 +55,7 @@ class Assert
      * @param  string  $description The message to include in the exception if the condition fails.
      *
      * @throws ParameterAssertionException if $condition is not true.
-     * @psalm-assert-true-condition $condition
+     * @psalm-assert bool $condition
      */
     public static function parameter($condition, $name, $description): void
     {
@@ -157,7 +157,7 @@ class Assert
      * @param  string  $name
      *
      * @throws ParameterTypeException if $value is not a non-empty string.
-     * @psalm-assert non-empty-string $value
+     * @psalm-assert string $value
      */
     public static function nonEmptyString($value, $name): void
     {
@@ -179,7 +179,7 @@ class Assert
      * @param  string  $description The message to include in the exception if the condition fails.
      *
      * @throws PostconditionException
-     * @psalm-assert-true-condition $condition
+     * @psalm-assert bool $condition
      */
     public static function postcondition($condition, $description): void
     {
@@ -206,7 +206,7 @@ class Assert
      * @param  string  $description The message to include in the exception if the condition fails.
      *
      * @throws InvariantException
-     * @psalm-assert-true-condition $condition
+     * @psalm-assert bool $condition
      */
     public static function invariant($condition, $description): void
     {
