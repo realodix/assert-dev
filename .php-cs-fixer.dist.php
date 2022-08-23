5dff1b82958210ae25c8a73aed80b60175c9563b
Realodix <budi03@gmail.com>
2022-08-23 11:51:12 +0700

    formatting

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,9 @@
 <?php
 
-use Realodix\CsConfig\Config;
-use Realodix\CsConfig\Finder;
-use Realodix\CsConfig\Rules\Realodix;
+use Realodix\Relax\Config;
 
-$overrides = [
+$localRules = [
     'binary_operator_spaces' => ['operators' => ['=>' => 'align_single_space_minimal']],
 ];
 
-$finder = Finder::base(__DIR__)
-    ->append(['.php-cs-fixer.dist.php']);
-
-return Config::create(new Realodix($overrides))
-    ->setFinder($finder);
+return Config::create('@Realodix', $localRules);
--- src/ParameterAssertionException.php
+++ src/ParameterAssertionException.php
@@ -2,13 +2,11 @@
 
 namespace Realodix\Assert;
 
-use InvalidArgumentException;
-
 /**
  * Exception indicating that an parameter assertion failed.
  * This generally means a disagreement between the caller and the implementation of a function.
  */
-class ParameterAssertionException extends InvalidArgumentException implements AssertionException
+class ParameterAssertionException extends \InvalidArgumentException implements AssertionException
 {
     /**
      * @var string
