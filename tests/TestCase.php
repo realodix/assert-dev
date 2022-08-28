996af52c61a17027826a1870bb85ea215e5440b1
Realodix <budi03@gmail.com>
2022-08-28 21:15:17 +0700

    Put the testFailed() on the TestCase.php file

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Realodix\Assert\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Realodix\Assert\Assert;
+
+class TestCase extends PHPUnitTestCase
+{
+    protected function testFailed($type, $value)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Assert::isType($type, $value);
+    }
+}
