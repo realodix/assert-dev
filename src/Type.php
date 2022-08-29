53c46d952c7d3f00c7cc291f1032b15fc7120093
Realodix <budi03@gmail.com>
2022-08-29 14:01:11 +0700

    Update Type.php

--- src/Type.php
+++ src/Type.php
@@ -12,7 +12,7 @@ class Type
      *                      type or a class or interface, or a list of such names.
      * @param mixed  $value The parameter's actual value.
      *
-     * @throws InvalidArgumentException if $value is not of type (or, for objects, is not an
+     * @throws \InvalidArgumentException if $value is not of type (or, for objects, is not an
      *                                  instance of) $type.
      */
     public static function is(string $types, $value, string $message = ''): void
