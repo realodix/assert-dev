9d0ccf1c37647255ebb9d0588dd3f34911f5a748
Realodix <budi03@gmail.com>
2022-09-07 01:01:59 +0700

    U-220907010158

--- tests/TypeTestProvider.php
+++ tests/TypeTestProvider.php
@@ -4,6 +4,7 @@ namespace Realodix\Assert\Tests;
 
 use Realodix\Assert\Tests\Fixtures\ClassAB;
 use Realodix\Assert\Tests\Fixtures\InterfaceA;
+use Realodix\Assert\Tests\Fixtures\InterfaceB;
 use Realodix\Relax\RuleSet\RuleSetInterface;
 use Realodix\Relax\RuleSet\Sets\Realodix;
 
