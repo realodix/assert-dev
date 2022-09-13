25be4e4ad45882e4a44b4628579a5d4f945311e4
Realodix <budi03@gmail.com>
2022-09-13 19:39:19 +0700

    Update ExceptionMessageTest.php

--- tests/ExceptionMessageTest.php
+++ tests/ExceptionMessageTest.php
@@ -10,7 +10,7 @@ class ExceptionMessageTest extends TestCase
     public function testExceptionMessage()
     {
         $this->expectExceptionMessage('Expected a string. Got: integer.');
-        Assert::type('string', 1);
+        Assert::type(1, 'string');
 
         $this->expectExceptionMessage('Expected an int. Got: string.');
         Assert::type('int', '1');
