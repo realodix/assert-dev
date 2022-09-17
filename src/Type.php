d789ef1dcb893ac7db412b4f25ffee668440e9e9
Realodix <budi03@gmail.com>
2022-09-17 21:42:29 +0700

    init

--- src/Type.php
+++ src/Type.php
@@ -118,4 +118,67 @@ class Type
             || ('int' == $allowedTypes) && \is_int($value)
             || ('float' == $allowedTypes) && \is_float($value);
     }
+
+    /**
+     * @param string|array $types
+     */
+    public static function type_has_duplicate($types): bool
+    {
+        Helper::assertStringOrArray($types, '$types');
+
+        if (\is_string($types)) {
+            $types = explode('|', $types);
+        }
+
+        if (\in_array('scalar', $types) &&
+                (\in_array('numeric', $types)
+                || \in_array('int', $types)
+                || \in_array('float', $types)
+                || \in_array('string', $types)
+                || \in_array('bool', $types))
+            || \in_array('numeric', $types) &&
+                (\in_array('int', $types)
+                || \in_array('float', $types))) {
+            return true;
+        }
+
+        // Tidak boleh ada 2 nama tipe atau lebih dalam satu deklarasi yang sama.
+        $actualTypesCount = \count($types);
+        $expectedTypesCount = \count(array_unique($types));
+        if ($expectedTypesCount < $actualTypesCount) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string|array $types
+     */
+    public static function normalize_type($types): array
+    {
+        Helper::assertStringOrArray($types, '$types');
+
+        if (\is_string($types)) {
+            $types = explode('|', $types);
+        }
+
+        return array_map(
+            function ($type) {
+                switch ($type) {
+                    case 'double':
+                        return 'float';
+                    case 'integer':
+                        return 'int';
+                    case 'boolean':
+                        return 'bool';
+                    case 'NULL':
+                        return 'null';
+                    default:
+                        return $type;
+                }
+            },
+            $types
+        );
+    }
 }
