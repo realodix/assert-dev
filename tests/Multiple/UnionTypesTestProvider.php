d11e864e21bc784ed1f56f466e05fd589b3b2f6f
Realodix <budi03@gmail.com>
2022-09-18 02:34:19 +0700

    Update UnionTypesTestProvider.php

--- tests/Multiple/UnionTypesTestProvider.php
+++ tests/Multiple/UnionTypesTestProvider.php
@@ -70,7 +70,21 @@ trait UnionTypesTestProvider
         return [
             ['bool|bool', true],
             ['bool|string|bool', true],
+
+            ['scalar|numeric', 123],
+            ['scalar|int', 123],
+            ['scalar|float', 123],
+            ['scalar|string', 123],
+            ['scalar|bool', 123],
+            ['numeric|int', 123],
+            ['numeric|float', 123],
+
+            ['bool|bool', true],
+            ['bool|string|bool', true],
+
             ['bool|boolean', true],
+            ['float|double', 1.23],
+            ['int|integer', 123],
         ];
     }
 
