7bf1c4aca0496d6d4683508433d78998f11ef38a
Kei <budi03@gmail.com>
2022-09-20 00:53:05 +0700

    Type: Array is ... (#32)

---
+++ src/Constraint.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Realodix\Assert;
+
+class Constraint
+{
+    /**
+     * @param mixed $value
+     */
+    public static function arrayIs($value, callable $callback): bool
+    {
+        $result = true;
+
+        if (! \is_array($value) || empty($value)) {
+            return false;
+        }
+
+        foreach ($value as $val) {
+            if (! $callback($val)) {
+                $result = false;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * https://www.php.net/manual/en/function.array-is-list.php
+     *
+     * @param mixed $value
+     */
+    public static function arrayIsList($value): bool
+    {
+        if (! \is_array($value)) {
+            return false;
+        }
+
+        return array_is_list($value);
+    }
+}
