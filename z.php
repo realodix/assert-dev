12f7eb4e28afed0794d829626ba5e96512e70873
Realodix <budi03@gmail.com>
2022-09-25 04:34:08 +0700

    Update z.php

--- z.php
+++ z.php
@@ -4,12 +4,20 @@ Assert::isInstanceOf($nodes, DOMNodeList::class);
 Assert::isInstanceOfAny();
 Assert::allIsAOf($compilerPasses, CompilerPassInterface::class);
 Assert::allIsInstanceOf($tags, Tag::class);
-Assert::inArray();
+Assert::oneOf($mutatorClass, ProfileList::ALL_MUTATORS);
 
 Assert::allFile($configFiles);
 Assert::allNotNull();
 Assert::allString($directories);
 Assert::allStringNotEmpty();
+
+Assert::notEmpty();
+Assert::notNull($descriptionFactory);
+Assert::null($this->microTime, 'Timer can not be started again without stopping.');
+Assert::nullOrIntegerish($lineCount);
+Assert::nullOrNotEmpty($version);
+
+Assert::inArray();
 Assert::boolean();
 Assert::classExists($handler);
 Assert::eq($analyzer->getNumberOfArguments($contructor), 0);
@@ -18,13 +26,7 @@ Assert::implementsInterface($handler, Tag::class);
 Assert::integerish($startingLine);
 Assert::isCallable($callable);
 Assert::maxLength($name, self::MAX_LENGTH);
-Assert::notEmpty();
 Assert::notEndsWith($name, ' ');
-Assert::notNull($descriptionFactory);
-Assert::null($this->microTime, 'Timer can not be started again without stopping.');
-Assert::nullOrIntegerish($lineCount);
-Assert::nullOrNotEmpty($version);
-Assert::oneOf($mutatorClass, ProfileList::ALL_MUTATORS);
 Assert::range($statusCode, 200, 599, 'Expected an HTTP status code. Got "%s"');
 Assert::string($docblock);
 Assert::stringNotEmpty($docblock);
