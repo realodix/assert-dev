e1e383a819dfa5e66f6db06c1494f4a91e9efb03
Realodix <budi03@gmail.com>
2022-09-26 17:21:42 +0700

    U-220926172142

---
+++ tests/Competitor/CompetitorTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Realodix\Assert\Tests\Competitor;
+
+use Realodix\Assert\Assert;
+use Realodix\Assert\Type;
+use Realodix\Assert\Tests\TestCase;
+
+class CompetitorTest extends TestCase
+{
+    use CompetitorTestProvider;
+
+    /**
+     * @dataProvider arrayProvider
+     */
+    public function testArray($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @dataProvider numberProvider
+     */
+    public function testNumber($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @dataProvider objectProvider
+     */
+    public function testObject($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider isBoolProvider
+     */
+    public function is_bool($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider instanceofProvider
+     */
+    public function instanceof($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /** @test */
+    public function is_null()
+    {
+        Assert::type(null, 'null');
+        Assert::type(null, 'NULL');
+        $this->addToAssertionCount(2);
+    }
+
+    /**
+     * @test
+     * @dataProvider isScalarProvider
+     */
+    public function is_scalar($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider isStringProvider
+     */
+    public function is_string($types, $value)
+    {
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider arrayIsProvider
+     */
+    public function arrayIs($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider nonEmptyProvider
+     */
+    public function nonEmpty($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidType($value, $types);
+
+        Assert::type($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider elementProvider
+     */
+    public function inArray($types, $value, $pass = true)
+    {
+        (! $pass) && $this->invalidElementType($value, $types);
+
+        Type::inArray($value, $types);
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @test
+     * @dataProvider arrayIsWithInvalidInputProvider
+     */
+    public function arrayIsWithInvalidInput($types, $value)
+    {
+        $this->invalidType($value, $types);
+    }
+}
