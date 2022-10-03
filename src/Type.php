165c02f2ae902bc30586c27fdb23e79c33ff904c
Realodix <budi03@gmail.com>
2022-10-03 18:28:59 +0700

    Update Type.php

--- src/Type.php
+++ src/Type.php
@@ -136,6 +136,8 @@ class Type
             || ('positive-int' === $allowedTypes) && \is_int($value) && $value >= 1
             || ('negative-int' === $allowedTypes) && \is_int($value) && $value <= -1
             || ('float' === $allowedTypes) && \is_float($value)
+            // ...-string
+            || ('numeric-string' === $allowedTypes) && is_numeric($value) && is_string($value)
             // non-empty-...
             || ('non-empty-string' === $allowedTypes) && Constraint::nonEmptyString($value)
             || ('non-empty-array' === $allowedTypes) && Constraint::nonEmptyArray($value)
