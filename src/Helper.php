85f46d21b6558b07d5a554f0c1460d70bd93b37a
Realodix <budi03@gmail.com>
2022-10-04 18:19:24 +0700

    Update Helper.php

--- src/Helper.php
+++ src/Helper.php
@@ -189,21 +189,23 @@ class Helper
             || \in_array('int', $types) &&
                 (\in_array('positive-int', $types)
                 || \in_array('negative-int', $types))
-            || \in_array('array', $types) &&
-                (\in_array('bool[]', $types)
+            || \in_array('array', $types) && (
+                \in_array('bool[]', $types)
                 || \in_array('string[]', $types)
                 || \in_array('int[]', $types)
                 || \in_array('float[]', $types)
                 || \in_array('object[]', $types)
                 || \in_array('list[]', $types)
                 || \in_array('non-empty-array', $types)
-                || \in_array('non-empty-list', $types))
+                || \in_array('non-empty-list', $types)
+            )
             || \in_array('non-empty-array', $types) &&
                 (\in_array('list[]', $types) || (\in_array('non-empty-list', $types)))
             || \in_array('list[]', $types) && \in_array('non-empty-list', $types)
             || \in_array('string', $types) && (
                 \in_array('non-empty-string', $types)
-                || \in_array('lowercase-string', $types))
+                || \in_array('lowercase-string', $types)
+            )
         ) {
             return true;
         }
