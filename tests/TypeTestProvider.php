cbd8ec4b351601d40f2d4d62dea7fd9abbfd59e2
Realodix <budi03@gmail.com>
2022-10-04 13:41:42 +0700

    Update TypeTestProvider.php

--- tests/TypeTestProvider.php
+++ tests/TypeTestProvider.php
@@ -242,9 +242,7 @@ trait TypeTestProvider
 
             ['object[]', [new \stdClass]],
             ['object[]', [new \RuntimeException]],
-            ['object[]', [null], false],
-            ['object[]', [true], false],
-            ['object[]', [1], false],
+            ['object[]', new \stdClass, false],
         ];
     }
 
