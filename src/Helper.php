d1c1797919d3868e09d65a1d05f71f079a68fa76
Realodix <budi03@gmail.com>
2022-10-06 03:53:33 +0700

    U-221006035333

--- src/Helper.php
+++ src/Helper.php
@@ -57,38 +57,6 @@ class Helper
     }
 
     /**
-     * @throws \ErrorException
-     * @throws Exception\UnknownClassOrInterfaceException
-     */
-    public static function assertIntersectionTypeMember(array $values): void
-    {
-        foreach ($values as $value) {
-            if (\is_string($value) && preg_match('/\\\/', $value) === 1
-                && ! interface_exists($value) && ! class_exists($value)) {
-                // https://github.com/flashios09/php-union-types/blob/master/src/Exception/ClassNotFoundException.php
-                throw new Exception\UnknownClassOrInterfaceException;
-            }
-
-            if (! interface_exists($value) && ! class_exists($value)) {
-                throw new \ErrorException(
-                    'Only class and interface can be part of an intersection type.'
-                );
-            }
-        }
-
-        $actTypesCount = \count($values);
-        $expTypesCount = \count(
-            array_intersect_key($values, array_unique(array_map('strtolower', $values)))
-        );
-
-        if ($expTypesCount < $actTypesCount) {
-            throw new \ErrorException(
-                'Duplicate type names in the same declaration is not allowed.'
-            );
-        }
-    }
-
-    /**
      * Periksa deklarasi format tipe. Ini harus dapat memastikan format yang
      * diberikan merupakan format yang valid.
      *
@@ -134,6 +102,38 @@ class Helper
         if (self::typeHasRedundantMembers($types)) {
             throw new \ErrorException(
                 'Type declarations has redundant types.'
+            );
+        }
+    }
+
+    /**
+     * @throws \ErrorException
+     * @throws Exception\UnknownClassOrInterfaceException
+     */
+    public static function assertIntersectionTypeMember(array $values): void
+    {
+        foreach ($values as $value) {
+            if (\is_string($value) && preg_match('/\\\/', $value) === 1
+                && ! interface_exists($value) && ! class_exists($value)) {
+                // https://github.com/flashios09/php-union-types/blob/master/src/Exception/ClassNotFoundException.php
+                throw new Exception\UnknownClassOrInterfaceException;
+            }
+
+            if (! interface_exists($value) && ! class_exists($value)) {
+                throw new \ErrorException(
+                    'Only class and interface can be part of an intersection type.'
+                );
+            }
+        }
+
+        $actTypesCount = \count($values);
+        $expTypesCount = \count(
+            array_intersect_key($values, array_unique(array_map('strtolower', $values)))
+        );
+
+        if ($expTypesCount < $actTypesCount) {
+            throw new \ErrorException(
+                'Duplicate type names in the same declaration is not allowed.'
             );
         }
     }
