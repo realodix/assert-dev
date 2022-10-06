95e254214f9e6141c86e13b5f8d8e958fe7e83f4
Realodix <budi03@gmail.com>
2022-10-07 04:23:30 +0700

    Update TypeTest.php

--- tests/TypeTest.php
+++ tests/TypeTest.php
@@ -154,4 +154,16 @@ class TypeTest extends TestCase
     {
         $this->invalidType($value, $types);
     }
+
+    /**
+     * @test
+     */
+    public function zzz()
+    {
+        $this->assertFalse((bool)'');
+        $this->assertFalse((bool)'0');
+        $this->assertFalse((bool)"0");
+
+        $this->assertTrue(empty('0'));
+    }
 }
