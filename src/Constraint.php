9df35a37c180f8107c1542e291b8c222a6d68901
Realodix <budi03@gmail.com>
2022-10-07 20:27:31 +0700

    U-221007202731

--- src/Constraint.php
+++ src/Constraint.php
@@ -6,6 +6,8 @@ class Constraint
 {
     /**
      * @param mixed $value
+     *
+     * @psalm-assert-if-false !list[] $value
      */
     public static function arrayIs($value, callable $callback): bool
     {
@@ -32,6 +34,8 @@ class Constraint
      * https://www.php.net/manual/en/function.array-is-list.php
      *
      * @param mixed $value
+     *
+     * @psalm-assert-if-false !list[] $value
      */
     public static function arrayIsList($value): bool
     {
