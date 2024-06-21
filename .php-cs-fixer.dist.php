74d7d6d1b32318f6f72b3a91a49a5df4200c1df6
Kei <budi03@gmail.com>
2024-06-22 01:08:28 +0700

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,11 @@
 <?php
 
 use Realodix\Relax\Config;
+use Realodix\Relax\RuleSet\Sets\Realodix;
 
 $localRules = [
     'binary_operator_spaces' => ['operators' => ['=>' => 'align_single_space_minimal']],
     'native_function_invocation' => true
 ];
 
-return Config::create('@Realodix', $localRules);
+return Config::create(new Realodix, $localRules);
